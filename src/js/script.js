94dc48ea7563371cb03d8e12858b6ecaa96a3c97
Pedro Parro <pedrohcparro@gmail.com>
2026-04-25 12:20:47 -0300

    LuteLivre

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,14 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+let attackCooldown = 0;
+let imagesLoaded = 0;
+
 let playerImg = new Image();
 playerImg.src = "src/images/player.png"
+
+let enemyImg = new Image();
+enemyImg.src = "src/images/enemy.png"
 
 let player = {
     x: 50,
@@ -17,11 +23,22 @@ let player = {
     grounded: false
 };
 
+let enemy = {
+    x: 550,
+    y: 50,
+    width: 50,
+    height: 50,
+    vx: 0,
+    vy: 0,
+    gravity: 0.5,
+    life: 100,
+}
+
 let chao = {
     x: 0,
     y: canvas.height / 2,
     width: canvas.width,
-    height: 50
+    height: 200
 };
 
 let keys = {};
@@ -30,36 +47,42 @@ document.addEventListener("keydown", e => keys[e.code] = true);
 document.addEventListener("keyup", e => keys[e.code] = false);
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     //Chão
-    ctx.fillStyle = "red";
-    ctx.fillRect(chao.x, chao.y, chao.width, chao.height)
+    ctx.fillStyle = "gray";
+    ctx.fillRect(chao.x, chao.y, chao.width, chao.height);
 
     //Player
-    ctx.drawImage(playerImg, player.x, player.y, player.width, player.height)
+    ctx.drawImage(playerImg, player.x, player.y, player.width, player.height);
+
+    //Inimigo
+    ctx.drawImage(enemyImg, enemy.x, enemy.y, enemy.width, enemy.height);
 };
 
 function update() {
     //Movimento horizontal
-    if (keys["ArrowRight" ]) player.vx = player.speed;
-    else if (keys["ArrowLeft"]) player.vx = -player.speed;
+    if (keys["KeyD" ]) player.vx = player.speed;
+    else if (keys["KeyA"]) player.vx = -player.speed;
     else player.vx = 0;
 
     //Pulo
-    if (keys["Space"] && player.grounded) {
+    if (keys["KeyW"] && player.grounded) {
         player.vy = player.jump;
         player.grounded = false;
     }
 
-    //Gravidade
-    player.vy += player.gravity;
-
-    //Atualizar posição
+    //Gravidade Player
+    player.vy += player.gravity
     player.x += player.vx;
     player.y += player.vy;
 
-    //Colisão com chão
+    //Gravidade Enemy
+    enemy.vy = enemy.vy || 0;
+    enemy.vy += enemy.gravity;
+    enemy.y += enemy.vy;
+
+    //Colisão com chão Player
     if (
         player.y + player.height >= chao.y &&
         player.y + player.height <= chao.y + chao.height &&
@@ -71,6 +94,29 @@ function update() {
     } else {
         player.grounded = false;
     }
+
+    //Colisão com o chão Enemy
+    if (enemy.y + enemy.height >= chao.y) {
+        enemy.y = chao.y - enemy.height;
+        enemy.vy = 0;
+    }
+
+    //Cooldown
+    if (attackCooldown > 0) attackCooldown--;
+
+    //Ataque
+    if (keys["KeyK"] && attackCooldown === 0) {
+        if (
+            player.x < enemy.x + enemy.width &&
+            player.x + player.width > enemy.x &&
+            player.y < enemy.y + enemy.height &&
+            player.y + player.height > enemy.y
+        ) {
+            enemy.life -= 1;
+            console.log("Acertou", enemy.life);
+            attackCooldown = 20;
+        }
+    }
 };
 
 function loop() {
@@ -80,6 +126,10 @@ function loop() {
     requestAnimationFrame(loop);
 };
 
-playerImg.onload = () => {
-    loop();
-};
+function checkStart() {
+    imagesLoaded++;
+    if (imagesLoaded === 2) loop()
+}
+
+playerImg.onload = checkStart;
+enemyImg.onload = checkStart;
